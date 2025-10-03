74deba10964f0bb8b4ac71bdc0bab94fc95b18f2
Bianca <myname45@gmail.com>
2025-10-03 10:55:12 -0400

    completely deleted some helper functions. converted some helper fxns to main logic. got rid of magic numbers. tie breaker now its own helper fxn.

--- adagrams/game.py
+++ adagrams/game.py
@@ -2,18 +2,69 @@ from random import randint
 from adagrams.homemade_functions import*
 
 def draw_letters():
-    chosen_letters = draw_hand()
-    return chosen_letters
+    weighted_letter_pool = multiply_letter_pool(LETTER_POOL)
+    pool_length = len(weighted_letter_pool)
+    
+    drawn_letters = []
+    purgatory_hand = {}
+    drawn_letters_count = 0
+    
+    MAX_HAND_SIZE = 10
+    
+    while drawn_letters_count != MAX_HAND_SIZE:
+        random_index = random.randint(0, pool_length - 1)
+        letter = weighted_letter_pool[random_index]
+        
+        letter_count = 0
+        if letter in purgatory_hand:
+            letter_count = purgatory_hand[letter]
+        
+        if letter_count < LETTER_POOL[letter]:
+            drawn_letters.append(letter)
+            purgatory_hand[letter] = letter_count + 1
+            drawn_letters_count += 1
+            
+    return drawn_letters
 
 def uses_available_letters(word, letter_bank):
-    word_list = convert_to_list(word)
-    word_dict = convert_to_dictionary(word_list)
+    upper_word = word.upper()
+    word_dict = convert_to_dictionary(upper_word)
     hand_dict = convert_to_dictionary(letter_bank)
-    return compare_word_to_hand(word_dict, hand_dict)
+    
+    for letter in word_dict:
+        if (
+            letter not in hand_dict 
+            or word_dict[letter] > hand_dict[letter]
+            ):
+            return False
+    return True
     
 
 def score_word(word):
-    return scoring(word)
+    upper_word = word.upper()
+    word_played = convert_to_dictionary(upper_word)
+
+    running_score = 0
+    
+    if len(word) >= 7:
+        BONUS_SCORE = 8
+        running_score += BONUS_SCORE
+        
+    for letter, count in word_played.items():
+        running_score += count * SCORE_CHART[letter]
+    
+    return running_score
 
 def get_highest_word_score(word_list):
-    return highest_score(word_list)
+    winning_word = ''
+    high_score = 0
+    
+    for word in word_list:
+        current_score = score_word(word)
+        if current_score > high_score:
+            high_score = current_score
+            winning_word = word
+        elif current_score == high_score:
+            winning_word = tie_breaker(word, winning_word)
+                        
+    return winning_word, high_score
--- adagrams/homemade_functions.py
+++ adagrams/homemade_functions.py
@@ -2,101 +2,32 @@ import random
 from random import randint
 from adagrams.constants import*
 
-
-def convert_to_list(word):
-    deconstructed_word = []
-    for letter in word:
-        deconstructed_word.append(letter.upper())
-    return deconstructed_word
-
-
-def list_length(list_to_count):
-    count = 0
-    for i in list_to_count:
-        count += 1
-    return count
-
 def multiply_letter_pool(LETTER_POOL):
     weighted_letters =[]
     for letter, amount in LETTER_POOL.items():
-        grouped_letters = letter * amount
-        for letters in convert_to_list(grouped_letters):
-            weighted_letters.append(letters)
+        grouped_letters = [letter] * amount
+        weighted_letters.extend(grouped_letters)
     return weighted_letters
 
-
-def draw_hand():
-    final_letter_pool = multiply_letter_pool(LETTER_POOL)
-    pool_length = list_length(final_letter_pool)
-    drawn_letters = []
-    purgatory_hand = {}
-    drawn_letters_count = 0
-    while drawn_letters_count != 10:
-        random_index = random.randint(0, pool_length - 1)
-        letter = final_letter_pool[random_index]
-        
-        letter_count = 0
-        if letter in purgatory_hand:
-            letter_count = purgatory_hand[letter]
-        
-        if letter_count < LETTER_POOL[letter]:
-            drawn_letters.append(letter)
-            purgatory_hand[letter] = letter_count + 1
-            drawn_letters_count += 1
-            
-    return drawn_letters
 
 def convert_to_dictionary(list):
     dictionary = {}
     for letter in list:
-        if letter in dictionary:
-            dictionary[letter] += 1
-        else:
-            dictionary[letter] = 1
+        dictionary[letter] = dictionary.get(letter, 0) + 1
     return dictionary
 
-def compare_word_to_hand(word_dict, hand_dict):
-    for letter in word_dict:
-        if letter not in hand_dict:
-            return False
-        elif word_dict[letter] > hand_dict[letter]:
-            return False
-    return True
 
-def scoring(word):
-    word_length = list_length(convert_to_list(word))
-    word_played = convert_to_dictionary(convert_to_list(word))
+def tie_breaker(current_word, winning_word):
+    length_current = len(current_word)
+    length_winning = len(winning_word)
     
-    score = 0
-    
-    if word_length >= 7:
-        score += 8
-    for letter, count in word_played.items():
-        for letters, points in SCORE_CHART.items():
-            if letter in letters:
-                score += word_played[letter] * SCORE_CHART[letters]
-                break
-    return score
-        
-    
-def highest_score(word_list):
-    winning_word = ''
-    high_score = 0
-    
-    for word in word_list:
-        current_score = scoring(word)
-        if current_score > high_score:
-            high_score = current_score
-            winning_word = word
-        elif current_score == high_score:
-            length_current_word = list_length(convert_to_list(word))
-            length_winning_word = list_length(convert_to_list(winning_word))
-            
-            if length_current_word == 10 and length_winning_word != 10:
-                winning_word = word
-            elif length_winning_word == 10 and length_current_word != 10: 
-                winning_word = winning_word
-            elif length_winning_word > length_current_word:
-                winning_word = word
-                
-    return winning_word, high_score
+    if length_current == 10 and length_winning != 10:
+        return current_word
+    elif length_winning == 10 and length_current != 10: 
+        return winning_word
+    #if neither word is 10 characters, shorter word wins
+    elif length_winning > length_current:
+        return current_word
+    #if length_current == length_highest
+    else:
+        return winning_word
